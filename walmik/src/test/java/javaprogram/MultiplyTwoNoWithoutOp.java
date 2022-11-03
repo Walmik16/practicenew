e93833266fad6f8f6aa785c532cf611369cd71c3
admin <admin@DESKTOP-6E87NOV>
2022-11-03 18:43:54 +0530

    first java program

---
+++ walmik/src/test/java/javaprogram/MultiplyTwoNoWithoutOp.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+public class MultiplyTwoNoWithoutOp {
+
+	// java program to multiply two number without using operator
+
+	public static void main(String[] args) {
+
+		int a = 20;
+		int b = 5;
+		int c = 0;
+		for (int i = 1; i <= 5; i++) {
+			c = c + a;
+		}
+		System.out.println(c);
+
+	}
+
+}
